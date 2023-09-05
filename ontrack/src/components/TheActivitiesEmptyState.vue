2d55c18dda6f9d9d1c20ea8e56a1923659b562d4
zohidulla <zohidjonkarimov@gmail.com>
2023-09-05 11:32:56 +0500

    component empty

---
+++ ontrack/src/components/TheActivitiesEmptyState.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div class="flex grow flex-col items-center gap-4 p-8 text-center">
+    <span class="text-gray-400">You don't have any activities! </span>
+    <img src="../assets/empty_page.svg" alt="No activities" class="h-full" />
+  </div>
+</template>
