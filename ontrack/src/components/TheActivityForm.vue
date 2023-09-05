393e3cc11e89ce303d345d83cb50170a0349b7f3
zohidulla <zohidjonkarimov@gmail.com>
2023-09-05 10:15:06 +0500

    ontrack: form component

---
+++ ontrack/src/components/TheActivityForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+import BaseButton from './BaseButton.vue'
+import { PlusIcon } from '@heroicons/vue/24/outline'
+import { isActivityValid } from '../validators'
+
+const emit = defineEmits({
+  submit: isActivityValid
+})
+
+let activity = ''
+
+function submit() {
+  emit('submit', activity)
+}
+</script>
+
+<template>
+  <form @submit.prevent="submit" class="sticky bottom-[57px] flex gap-2 bg-white p-4">
+    <input
+      type="text"
+      :value="activity"
+      @input="activity = $event.target.value"
+      class="w-full rounded border px-4 text-xl"
+      placeholder="Activity name"
+    />
+    <BaseButton>
+      <PlusIcon class="h-8" />
+    </BaseButton>
+  </form>
+</template>
--- ontrack/src/page/TheActivities.vue
+++ ontrack/src/page/TheActivities.vue
@@ -1,8 +1,7 @@
 <script setup>
 import { validateActivities, isActivityValid } from '../validators'
 import ActivityItem from '../components/ActivityItem.vue'
-import BaseButton from '../components/BaseButton.vue'
-import { PlusIcon } from '@heroicons/vue/24/outline'
+import TheActivityForm from '../components/TheActivityForm.vue'
 
 defineProps({
   activities: {
@@ -13,6 +12,7 @@ defineProps({
 })
 
 const emit = defineEmits({
+  createActivity: isActivityValid,
   deleteActivity: isActivityValid
 })
 </script>
@@ -27,11 +27,6 @@ const emit = defineEmits({
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
-    <form class="sticky bottom-[57px] flex gap-2 bg-white p-4">
-      <input type="text" class="w-full rounded border px-4 text-xl" placeholder="Activity name" />
-      <BaseButton>
-        <PlusIcon class="h-8" />
-      </BaseButton>
-    </form>
+    <TheActivityForm @submit="emit('createActivity', $event)" />
   </div>
 </template>
