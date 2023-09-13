ef2c51bfe4aa98e35629377d4f8fc539a70822c4
zohidulla <zohidjonkarimov@gmail.com>
2023-09-13 11:07:53 +0500

    stopwatch markings

--- ontrack/src/constants.js
+++ ontrack/src/constants.js
@@ -2,10 +2,18 @@ import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outli
 import { generatePeriodSelectOptions } from './functions'
 
 export const BUTTON_TYPE_PRIMARY = 'primary'
+export const BUTTON_TYPE_SUCCESS = 'success'
+export const BUTTON_TYPE_WARNING = 'warning'
 export const BUTTON_TYPE_NEUTRAL = 'neutral'
 export const BUTTON_TYPE_DANGER = 'danger'
 
-export const BUTTON_TYPES = [BUTTON_TYPE_PRIMARY, BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER]
+export const BUTTON_TYPES = [
+  BUTTON_TYPE_PRIMARY,
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
+  BUTTON_TYPE_NEUTRAL,
+  BUTTON_TYPE_DANGER
+]
 
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
