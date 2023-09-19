fb425417fa642ecbc8cc275a8c12ecf68b975da6
zohidulla <zohidjonkarimov@gmail.com>
2023-09-19 11:56:30 +0500

    Macros defineExpose

--- ontrack/src/App.vue
+++ ontrack/src/App.vue
@@ -19,9 +19,17 @@ const activities = ref(generateActivities())
 
 const timelineItems = ref(generateTimelineItems(activities.value))
 
+const timeline = ref()
+
 const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
 
 function goTo(page) {
+  if (currentPage.value === PAGE_TIMELINE && page === PAGE_TIMELINE) {
+    timeline.value.scrollToHour()
+  }
+  if (page !== PAGE_TIMELINE) {
+    document.body.scrollIntoView()
+  }
   currentPage.value = page
 }
 
@@ -58,6 +66,7 @@ function setActivitySecondsToComplete(activity, secondsToComplete) {
       :activities="activities"
       :activity-select-options="activitySelectOptions"
       :current-page="currentPage"
+      ref="timeline"
       @set-timeline-item-activity="setTimelineItemActivity"
     />
     <TheActivities
