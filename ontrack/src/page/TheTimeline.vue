54055480f1ec4a84f75f70202d1b0854bce0796c
zohidulla <zohidjonkarimov@gmail.com>
2023-09-21 14:37:21 +0500

    setTimelineItemActivity to provide/inject

--- ontrack/src/page/TheTimeline.vue
+++ ontrack/src/page/TheTimeline.vue
@@ -1,12 +1,7 @@
 <script setup>
 import { nextTick, ref, watchPostEffect } from 'vue'
 import TimelineItem from '../components/TimelineItem.vue'
-import {
-  isActivityValid,
-  isPageValid,
-  isTimelineItemValid,
-  validateTimelineItems
-} from '../validators'
+import { isPageValid, validateTimelineItems } from '../validators'
 import { MIDNIGHT_HOUR, PAGE_TIMELINE } from '../constants'
 
 const props = defineProps({
@@ -19,12 +14,6 @@ const props = defineProps({
     required: true,
     type: String,
     validator: isPageValid
-  }
-})
-
-const emit = defineEmits({
-  setTimelineItemActivity(timelineItem, activity) {
-    return [isTimelineItemValid(timelineItem), isActivityValid(activity)].every(Boolean)
   }
 })
 
@@ -59,7 +48,6 @@ function scrollToHour(hour = null, isSmooth = true) {
         :timeline-item="timelineItem"
         ref="timelineItemRefs"
         @scroll-to-hour="scrollToHour"
-        @select-activity="emit('setTimelineItemActivity', timelineItem, $event)"
       />
     </ul>
   </div>
