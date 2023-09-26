d83a088c42158ea9be6021e5fdee4578e1260360
zohidulla <zohidjonkarimov@gmail.com>
2023-09-26 11:11:09 +0500

    refactoring NavItem component

--- ontrack/src/components/TheNav.vue
+++ ontrack/src/components/TheNav.vue
@@ -1,18 +1,11 @@
 <script setup>
 import { NAV_ITEMS } from '../constants'
-import { navigate, currentPage } from '../router'
 import TheNavItem from './TheNavItem.vue'
 </script>
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <TheNavItem
-        v-for="(icon, page) in NAV_ITEMS"
-        :key="page"
-        :href="`#${page}`"
-        :class="{ 'pointer-events-none bg-gray-200': page === currentPage }"
-        @click="navigate(page)"
-      >
+      <TheNavItem v-for="(icon, page) in NAV_ITEMS" :key="page" :page="page">
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </TheNavItem>
     </ul>
