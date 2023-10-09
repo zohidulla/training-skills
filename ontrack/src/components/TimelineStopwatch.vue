88f740c47138d3b013dee987048f53dfff408f00
zohidulla <zohidjonkarimov@gmail.com>
2023-10-09 14:52:48 +0500

    filling the progress bar

--- ontrack/src/components/TimelineStopwatch.vue
+++ ontrack/src/components/TimelineStopwatch.vue
@@ -23,6 +23,7 @@ const props = defineProps({
 
 const seconds = ref(props.timelineItem.activitySeconds)
 const isRunning = ref(false)
+const temp = 120
 const isStartButtonDisabled = props.timelineItem.hour !== currentHour()
 
 watch(
@@ -35,7 +36,7 @@ watch(
 function start() {
   isRunning.value = setInterval(() => {
     updateTimelineItem(props.timelineItem, {
-      activitySeconds: props.timelineItem.activitySeconds + 1
+      activitySeconds: props.timelineItem.activitySeconds + temp
     })
     seconds.value++
   }, MILLISECONDS_IN_SECOND)
@@ -50,7 +51,7 @@ function stop() {
 function reset() {
   stop()
   updateTimelineItem(props.timelineItem, {
-    activitySeconds: props.timelineItem.activitySeconds - seconds.value
+    activitySeconds: props.timelineItem.activitySeconds - seconds.value * temp
   })
   seconds.value = 0
 }
