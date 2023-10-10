3eb3d004d2bfeca1a53f53cf7fbf982df1a3457b
zohidulla <zohidjonkarimov@gmail.com>
2023-10-10 15:13:01 +0500

    add: composable function

--- ontrack/src/components/ProgressItem.vue
+++ ontrack/src/components/ProgressItem.vue
@@ -1,8 +1,6 @@
 <script setup>
-import { computed } from 'vue'
-import { formatSeconds, getProgressColorClass } from '../functions'
-import { calculateActivityCompletionPercentage } from '../activities'
-import { calculateTrackedActivitySeconds, timelineItems } from '../timeline-items'
+import { formatSeconds } from '../functions'
+import { useProgress } from '../composables/progress'
 import { isActivityValid } from '../validators'
 
 const props = defineProps({
@@ -13,25 +11,19 @@ const props = defineProps({
   }
 })
 
-const percentage = computed(() =>
-  calculateActivityCompletionPercentage(props.activity, trackedActivitySeconds.value)
-)
-
-const trackedActivitySeconds = computed(() =>
-  calculateTrackedActivitySeconds(timelineItems.value, props.activity)
-)
+const { colorClass, percentage, trackedSeconds } = useProgress(props.activity)
 </script>
 
 <template>
   <li class="flex flex-col gap-1 p-4">
     <div class="truncate text-xl">{{ activity.name }}</div>
     <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
-      <div :class="getProgressColorClass(percentage)" :style="`width: ${percentage}%`" />
+      <div :class="colorClass" :style="`width: ${percentage}%`" />
     </div>
     <div class="flex justify-between font-mono text-sm">
       <span>{{ percentage }}%</span>
       <span>
-        {{ formatSeconds(trackedActivitySeconds) }} /
+        {{ formatSeconds(trackedSeconds) }} /
         {{ formatSeconds(activity.secondsToComplete) }}
       </span>
     </div>
