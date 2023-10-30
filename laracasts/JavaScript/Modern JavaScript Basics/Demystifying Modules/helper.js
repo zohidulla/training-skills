9abd5bf50910ba53335a1c9404fc8408eeb1bf2c
zohidulla <zohidjonkarimov@gmail.com>
2023-10-30 13:46:36 +0500

    Demystifying Modules

---
+++ laracasts/JavaScript/Modern JavaScript Basics/Demystifying Modules/helper.js
@@ -0,0 +1,3 @@
+export default (arr) => {
+  return arr.map((item) => item.toUpperCase());
+};
---
+++ laracasts/JavaScript/Modern JavaScript Basics/users.js
@@ -0,0 +1,18 @@
+let users = [
+  {
+    name: "Andrew",
+    email: "andrew@example.com"
+  },
+  {
+    name: "Ashley",
+    email: "ashley@example.com"
+  },
+  {
+    name: "Gaston",
+    email: "gaston@example.com"
+  }
+];
+
+let currentUser = 2;
+
+export { users, currentUser };
