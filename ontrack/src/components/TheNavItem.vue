eb12db015a566051297c4fa656623afe8e2a8118
zohidulla <zohidjonkarimov@gmail.com>
2023-11-13 10:25:09 +0500

    Navigation Item component improvement

--- ontrack/src/components/TheNavItem.vue
+++ ontrack/src/components/TheNavItem.vue
@@ -3,6 +3,8 @@ import { computed } from 'vue'
 import { currentPage, navigate } from '../router'
 import { isNavItemValid } from '../validators'
 import BaseIcon from './BaseIcon.vue'
+import { PAGE_TIMELINE } from '../constants'
+import { scrollToCurrentHour } from '../timeline-items'
 
 const props = defineProps({
   navItem: {
@@ -14,13 +16,19 @@ const props = defineProps({
 
 const classes = computed(() => [
   'flex flex-col items-center p-2 text-xs capitalize',
-  { 'pointer-events-none bg-gray-200': props.navItem.page === currentPage.value }
+  { 'bg-gray-200': props.navItem.page === currentPage.value }
 ])
+
+function handleClick() {
+  currentPage.value === PAGE_TIMELINE && props.navItem.page === PAGE_TIMELINE
+    ? scrollToCurrentHour(true)
+    : navigate(props.navItem.page)
+}
 </script>
 
 <template>
   <li class="flex-1">
-    <a :href="`#${navItem.page}`" :class="classes" @click="navigate(navItem.page)">
+    <a :href="`#${navItem.page}`" :class="classes" @click="handleClick">
       <BaseIcon :name="navItem.icon" class="h-6 w-6" /> {{ navItem.page }}
     </a>
   </li>
--- ontrack/src/page/TheTimeline.vue
+++ ontrack/src/page/TheTimeline.vue
@@ -1,10 +1,16 @@
 <script setup>
-import { onActivated } from 'vue'
+import { onActivated, onDeactivated } from 'vue'
 import { scrollToCurrentHour, timelineItemRefs, timelineItems } from '../timeline-items'
 import TimelineItem from '../components/TimelineItem.vue'
 import TheTimelineIndicator from '../components/TheTimelineIndicator.vue'
+import { startTimer, stopTimer } from '../time'
 
-onActivated(scrollToCurrentHour)
+onActivated(() => {
+  scrollToCurrentHour()
+  startTimer()
+})
+
+onDeactivated(stopTimer)
 </script>
 
 <template>
