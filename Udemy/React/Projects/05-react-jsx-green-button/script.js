a62e9d9a88b21e3a139889d81748005f3c28d963
zohidulla <zohidjonkarimov@gmail.com>
2024-05-22 16:00:30 +0500

    jsx react usestate

---
+++ Udemy/React/Projects/05-react-jsx-green-button/script.js
@@ -0,0 +1,22 @@
+const App = (props) => {
+  const { initialButtonText, initialClassesList } = props;
+  const [buttonText, setButtonText] = React.useState(initialButtonText);
+  const [classesList, setClassesList] = React.useState(initialClassesList);
+
+  const onButtonClick = () => {
+    setButtonText("Hello from React");
+    setClassesList("green-btn");
+  };
+
+  return (
+    <div className="app">
+      <button className={classesList} onClick={onButtonClick}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container);
+root.render(<App initialButtonText="Click me" initialClassesList="" />);
