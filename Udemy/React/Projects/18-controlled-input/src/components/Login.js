d55023de00855b7d012398382a7eb63d7f3187fb
zohidulla <zohidjonkarimov@gmail.com>
2024-06-02 12:20:11 +0500

    Control input

---
+++ Udemy/React/Projects/18-controlled-input/src/components/Login.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function Login() {
+  const [data, setData] = useState({ username: "", password: "" });
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    alert(JSON.stringify(data));
+  }
+
+  function handleInputChange(e, name) {
+    setData({ ...data, [name]: e.target.value });
+  }
+
+  return (
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={data.username}
+            onChange={(e) => handleInputChange(e, "username")}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={data.password}
+            onChange={(e) => handleInputChange(e, "password")}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
