edca93420e163e60290ffc1a80b5a0e09dbe119e
zohidulla <zohidjonkarimov@gmail.com>
2024-06-02 17:18:46 +0500

    useEffect async await

---
+++ Udemy/React/Projects/22-useEffect-async-await/src/components/Posts.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import Post from "./Post";
+
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // async function fetchData() {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(API_URL);
+        const posts = await res.json();
+        setPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // IIFE - Immediately Invoked Function Expression
+  // useEffect(() => {
+  //   (async function () {
+  //     try {
+  //       const res = await fetch(API_URL);
+  //       const posts = await res.json();
+  //       setPosts(posts);
+  //     } catch (error) {
+  //       setError(error.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   })();
+  // }, []);
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((response) => response.json())
+  //     .then((posts) => setPosts(posts))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => <Post {...post} key={post.id} />)
+      )}
+    </>
+  );
+}
+
+export default Posts;
