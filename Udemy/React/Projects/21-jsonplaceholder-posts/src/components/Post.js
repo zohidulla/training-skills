387dd0df9ad85aba76fa1036e430764886d04ae4
zohidulla <zohidjonkarimov@gmail.com>
2024-06-02 16:13:54 +0500

    useEffect jsonplaceholder posts

---
+++ Udemy/React/Projects/21-jsonplaceholder-posts/src/components/Post.js
@@ -0,0 +1,14 @@
+import "./Post.css";
+
+function Post({ id, title, body, userId }) {
+  return (
+    <div className="post">
+      <small>{id}</small>
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <h3>User ID: {userId}</h3>
+    </div>
+  );
+}
+
+export default Post;
