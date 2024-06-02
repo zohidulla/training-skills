32d7e559227f5e163244a439a69683ad9454626b
zohidulla <zohidjonkarimov@gmail.com>
2024-06-02 11:33:09 +0500

    Uncontrol input

---
+++ Udemy/React/Projects/17-uncontrol-input/src/components/Login.js
@@ -0,0 +1,30 @@
+function Login() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const userData = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+
+    alert(JSON.stringify(userData));
+  }
+  return (
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
