71f9e4a041c6a8fa7815d61827a6e5435fc67bb7
zohidulla <zohidjonkarimov@gmail.com>
2024-06-06 12:03:29 +0500

    pure Redux without React

---
+++ Udemy/React/Projects/29-pure-redux/index.js
@@ -0,0 +1,39 @@
+import store from "./redux/store.js";
+import { addCurrentTime, clearTimes } from "./redux/actionCreators.js";
+
+const addTimeButton = document.getElementById("addTime");
+addTimeButton.addEventListener("click", () => store.dispatch(addCurrentTime()));
+
+const clearTimesButton = document.getElementById("clearTimes");
+clearTimesButton.addEventListener("click", () => store.dispatch(clearTimes()));
+
+const timeList = document.getElementById("timesList");
+store.subscribe(() => {
+  timeList.innerHTML = "";
+  const times = store.getState();
+  times.forEach((time) => {
+    let li = document.createElement("li");
+    li.innerText = time;
+    timeList.appendChild(li);
+  });
+});
+
+// const unsubscribe = store.subscribe(() =>
+//   console.log(`Redux store just changed! ${store.getState()}`)
+// );
+
+// store.dispatch({
+//   type: "ADD_CURRENT_TIME",
+//   payload: "11:30:00",
+// });
+
+// unsubscribe();
+
+// store.dispatch({
+//   type: "ADD_CURRENT_TIME",
+//   payload: "11:32:00",
+// });
+
+// store.dispatch({
+//   type: "CLEAR_ALL_TIMES",
+// });
---
+++ Udemy/React/Projects/29-pure-redux/redux/reducer.js
@@ -0,0 +1,16 @@
+import * as actionTypes from "./ActionTypes.js";
+
+const initialState = [];
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.ADD_CURRENT_TIME:
+      return [...state, action.payload];
+    case actionTypes.CLEAR_ALL_TIMES:
+      return [];
+    default:
+      return state;
+  }
+}
+
+export default reducer;
