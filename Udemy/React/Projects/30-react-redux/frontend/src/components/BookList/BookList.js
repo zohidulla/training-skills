76697731159635f726bda265b1a319787d2585a7
zohidulla <zohidjonkarimov@gmail.com>
2024-06-06 19:24:37 +0500

    Project React with Redux - Book Library App

---
+++ Udemy/React/Projects/30-react-redux/frontend/src/components/BookList/BookList.js
@@ -0,0 +1,50 @@
+import { useDispatch, useSelector } from "react-redux";
+import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
+import { BsBookmarkStarFill, BsBookmarkStar } from "react-icons/bs";
+import "./BookList.css";
+
+const BookList = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleDeleteBook = (id) => {
+    dispatch(deleteBook(id));
+  };
+
+  const handleToggleFavorite = (id) => {
+    dispatch(toggleFavorite(id));
+  };
+
+  return (
+    <div className="app-block book-list">
+      <h2>Book List</h2>
+      {!books.length ? (
+        <p>No books available</p>
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={book.id}>
+              <div className="book-info">
+                {++i}. {book.title} by <strong>{book.author}</strong>
+              </div>
+              <div className="book-actions">
+                <span onClick={() => handleToggleFavorite(book.id)}>
+                  {book.isFavorite ? (
+                    <BsBookmarkStarFill className="star-icon" />
+                  ) : (
+                    <BsBookmarkStar className="star-icon" />
+                  )}
+                </span>
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default BookList;
