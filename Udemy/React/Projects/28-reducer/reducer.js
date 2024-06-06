da588103fe41abeec7693fc3f8dd90aa0e53073d
zohidulla <zohidjonkarimov@gmail.com>
2024-06-06 10:36:56 +0500

    reducer without Redux

---
+++ Udemy/React/Projects/28-reducer/reducer.js
@@ -0,0 +1,41 @@
+const initialState = [];
+
+function reducer(state, action) {
+  // // use switch
+  switch (action.type) {
+    case "ADD_NAME":
+      return [...state, action.payload];
+    case "DELETE_NAME":
+      return state.filter((personName) => personName !== action.payload);
+    case "CLEAR_NAMES":
+      return [];
+    default:
+      return state;
+  }
+  // // use if
+  // if (action.type === "ADD_NAME") {
+  //   return [...state, action.payload];
+  // }
+  // if (action.type === "DELETE_NAME") {
+  //   return state.filter((personName) => personName !== action.payload);
+  // }
+  // if (action.type === "CLEAR_NAMES") {
+  //   return [];
+  // }
+  // return state;
+}
+
+let newState = reducer(initialState, { type: "ADD_NAME", payload: "John" });
+console.log(newState);
+
+newState = reducer(newState, { type: "ADD_NAME", payload: "Alice" });
+console.log(newState);
+
+newState = reducer(newState, { type: "DELETE_NAME", payload: "Alice" });
+console.log(newState);
+
+newState = reducer(newState, { type: "CLEAR_NAMES" });
+console.log(newState);
+
+newState = reducer(initialState, { type: "ADD_NAME", payload: "Bob" });
+console.log(newState);
