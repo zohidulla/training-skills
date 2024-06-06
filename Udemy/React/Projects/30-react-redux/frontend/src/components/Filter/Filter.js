dddf825387ed1f54faca6cce7c2a051d13c62e41
zohidulla <zohidjonkarimov@gmail.com>
2024-06-06 19:03:15 +0500

    Project React with Redux - Book Library App

---
+++ Udemy/React/Projects/30-react-redux/frontend/src/components/Filter/Filter.js
@@ -0,0 +1,13 @@
+import "./Filter.css";
+
+const Filter = () => {
+  return (
+    <div className="app-block filter">
+      <div className="filter-group">
+        <input type="text" placeholder="Filter by title..." />
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
