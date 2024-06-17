278ca652eae01e44df9105ec8d30dbdae8f68279
zohidulla <zohidjonkarimov@gmail.com>
2024-06-17 19:12:42 +0500

    Another option for extraReducer

--- Udemy/React/Projects/30-react-redux/frontend/src/redux/slices/booksSlice.js
+++ Udemy/React/Projects/30-react-redux/frontend/src/redux/slices/booksSlice.js
@@ -45,6 +45,15 @@ const booksSlice = createSlice({
       });
     },
   },
+  // // OPTION 1
+  // extraReducers: {
+  //   [fetchBook.fulfilled]: (state, action) => {
+  //     if (action.payload.title && action.payload.author) {
+  //       state.push(createBookWithID(action.payload, "API"));
+  //     }
+  //   },
+  // },
+  // OPTION 2
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
       if (action.payload.title && action.payload.author) {
