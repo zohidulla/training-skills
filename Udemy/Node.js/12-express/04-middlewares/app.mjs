22bb6a37ddfa56a613bb280e3dfcf252b1c433d9
zohidulla <zohidjonkarimov@gmail.com>
2024-07-08 11:32:38 +0500

    Express middlewares

--- Udemy/Node.js/12-express/04-middlewares/app.mjs
+++ Udemy/Node.js/12-express/04-middlewares/app.mjs
@@ -1,14 +1,43 @@
 import express from "express";
+import morgan from "morgan";
+// import qs from "querystring";
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(req.method, req.path);
-  next();
-};
+// logs info about request
+app.use(morgan("tiny")); // middleware function1
 
-app.use(logger);
+// converts JSON to JS Object in POST, PUT, PATCH requests
+app.use(express.json()); // middleware function2
+// app.use((req, res, next) => {
+//   let data = "";
+//   req.on("data", (chunk) => (data += chunk));
+//   req.on("end", () => {
+//     const parsedJSON = JSON.parse(data);
+//     req.body = parsedJSON;
+//     next();
+//   });
+// });
 
-app.use((req, res) => res.send("This is express server"));
+// converts from data to JS Object in POST, PUT, PATCH requests
+app.use(express.urlencoded({ extended: true })); // middleware function3
+// app.use((req, res, next) => {
+//   if (req.headers["content-type"] === "application/x-www-form-urlencoded") {
+//     let data = "";
+//     req.on("data", (chunk) => (data += chunk.toString()));
+//     req.on("end", () => {
+//       const parsedFormData = qs.parse(data);
+//       req.body = parsedFormData;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((req, res) => {
+  console.log(req.body);
+  return res.send("This is express server");
+});
 
 app.listen(5000, () => console.log("Server is listening at port 5000"));
