f827f705a89ab7c6698f5c065ed1769988b6a592
zohidulla <zohidjonkarimov@gmail.com>
2024-10-03 08:18:18 +0500

    Convert router.js

--- ontrack-typescript/src/validators.js
+++ ontrack-typescript/src/validators.js
@@ -1,5 +1,5 @@
-import { BUTTON_TYPES, HOURS_IN_DAY, MIDNIGHT_HOUR, NAV_ITEMS } from './constants.js'
-import { ICONS } from './icons.js'
+import { BUTTON_TYPES, HOURS_IN_DAY, MIDNIGHT_HOUR, NAV_ITEMS } from './constants.ts'
+import { ICONS } from './icons.ts'
 
 export function isPageValid(page) {
   return NAV_ITEMS.some((navItem) => navItem.page === page)
