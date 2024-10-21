eb2829c9432dbc233b697f56c2b3517fc1811a07
zohidulla <zohidjonkarimov@gmail.com>
2024-10-21 08:05:33 +0500

    Generic component

--- ontrack-typescript/src/components/BaseSelect.vue
+++ ontrack-typescript/src/components/BaseSelect.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="T extends number | string">
 import { computed } from 'vue'
 import BaseButton from './BaseButton.vue'
 import { isUndefinedOrNull } from '../validators'
@@ -6,16 +6,17 @@ import { BUTTON_TYPE_NEUTRAL } from '../constants'
 import { normalizeSelectValue } from '../functions'
 import BaseIcon from './BaseIcon.vue'
 import { ICON_X_MARK } from '../icons'
+import type { SelectOption } from '../types'
 
 const props = defineProps<{
-  options: { value: number | string; label: string }[]
-  selected: number | string | null
+  options: SelectOption<T>[]
+  selected: T | null
   placeholder: string
 }>()
 
 const emit = defineEmits<{
   // (e: 'select', value: number | string | null): void
-  select: [value: number | string | null]
+  select: [value: T | null]
 }>()
 
 const isNotSelected = computed((): boolean => isUndefinedOrNull(props.selected))
