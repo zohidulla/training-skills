4f98fae1468c80518090ca8cc40d5839df8dda78
zohidulla <zohidjonkarimov@gmail.com>
2024-10-31 09:45:46 +0500

    Change type any

--- ontrack-typescript/src/time.ts
+++ ontrack-typescript/src/time.ts
@@ -11,7 +11,7 @@ export const now = ref(today())
 const midnight = computed((): number => new Date(now.value).setHours(0, 0, 0, 0))
 
 const secondsSinceMidnight = computed(
-  (): number => ((now.value as any) - midnight.value) / MILLISECONDS_IN_SECOND
+  (): number => (now.value.getTime() - midnight.value) / MILLISECONDS_IN_SECOND
 )
 
 export function today(): Date {
--- ontrack-typescript/src/timeline-items.ts
+++ ontrack-typescript/src/timeline-items.ts
@@ -90,16 +90,16 @@ function resetTimelineItems(): void {
 }
 
 function syncIdleSeconds(lastActiveAt: Date): void {
-  updateTimelineItem(activeTimelineItem.value as any, {
-    activitySeconds:
-      (activeTimelineItem.value as any).activitySeconds + calculateIdleSeconds(lastActiveAt)
+  if (!activeTimelineItem.value) return
+  updateTimelineItem(activeTimelineItem.value, {
+    activitySeconds: activeTimelineItem.value.activitySeconds + calculateIdleSeconds(lastActiveAt)
   })
 }
 
 function calculateIdleSeconds(lastActiveAt: Date): number {
   return lastActiveAt.getHours() === today().getHours()
-    ? toSeconds((endOfHour(lastActiveAt) as any) - (lastActiveAt as any))
-    : toSeconds((today() as any) - (lastActiveAt as any))
+    ? toSeconds(today().getTime() - lastActiveAt.getTime())
+    : toSeconds(endOfHour(lastActiveAt).getTime() - lastActiveAt.getTime())
 }
 
 function filterTimelineItemsByActivity(
