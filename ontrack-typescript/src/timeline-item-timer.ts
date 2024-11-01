5b736cfd2d67881dfb1b066a07c93e49410803de
zohidulla <zohidjonkarimov@gmail.com>
2024-11-01 08:18:36 +0500

    Convert vite.config.js

--- ontrack-typescript/src/timeline-item-timer.ts
+++ ontrack-typescript/src/timeline-item-timer.ts
@@ -3,7 +3,7 @@ import { activeTimelineItem, updateTimelineItem } from './timeline-items'
 import { MILLISECONDS_IN_SECOND } from './constants'
 import type { TimelineItem } from './types'
 
-const timelineItemTimer = ref<number | undefined>()
+const timelineItemTimer = ref<NodeJS.Timeout | undefined>()
 
 export function startTimelineItemTimer(timelineItem?: TimelineItem): void {
   timelineItem = timelineItem ?? activeTimelineItem.value
