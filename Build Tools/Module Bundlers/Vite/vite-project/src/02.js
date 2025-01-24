b17d52f59cf6c4206bce08211f95ce8367e6c45e
zohidulla <zohidjonkarimov@gmail.com>
2025-01-24 09:57:10 +0500

    CSS modules

---
+++ Build Tools/Module Bundlers/Vite/vite-project/src/02.js
@@ -0,0 +1,3 @@
+import collect from "collect.js";
+
+console.log(collect([1, 2, 3, 4, 5]).avg());
--- Build Tools/Module Bundlers/Vite/vite-project/src/main.js
+++ Build Tools/Module Bundlers/Vite/vite-project/src/main.js
@@ -1,24 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "../src/assets/03.css";
+import { heading as headingColor } from "../src/assets/04a.module.css";
+import { heading as headingFontSize } from "../src/assets/04b.module.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.querySelector(
+  ".heading"
+).className = `${headingColor} ${headingFontSize}`;
