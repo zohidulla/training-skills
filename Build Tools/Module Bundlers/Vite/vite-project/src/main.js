6a3eaff1c0797ac4f9856a057a75cf491fd36259
zohidulla <zohidjonkarimov@gmail.com>
2025-02-01 10:03:57 +0500

    Support jsx syntax

--- Build Tools/Module Bundlers/Vite/vite-project/src/main.js
+++ Build Tools/Module Bundlers/Vite/vite-project/src/main.js
@@ -1,15 +1,3 @@
-const modules = import.meta.glob("./src/10/*/js", {
-  eager: true,
-  import: "name",
-});
-// const modules = import.meta.glob("./src/10/*/js", { as: "url", eager: true });
+import { template } from "./12.jsx";
 
-console.log(modules);
-
-// document.addEventListener("click", () => {
-//   Object.values(modules).forEach((module) => {
-//     module().then((name) => {
-//       console.log(name);
-//     });
-//   });
-// });
+document.querySelector("#app").appendChild(template);
--- Build Tools/Module Bundlers/Vite/vite-project/vite.config.js
+++ Build Tools/Module Bundlers/Vite/vite-project/vite.config.js
@@ -3,5 +3,9 @@ import Inspect from "vite-plugin-inspect";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  esbuild: {
+    jsxFactory: "create",
+    jsxInject: "import { create } from '/src/12-create.js'",
+  },
   plugins: [Inspect(), tailwindcss()],
 });
