409167c08e5c6535272cc799ac8c42da669af149
zohidulla <zohidjonkarimov@gmail.com>
2025-02-02 14:14:20 +0500

    Typescript

--- Build Tools/Module Bundlers/Vite/vite-project/vite.config.js
+++ Build Tools/Module Bundlers/Vite/vite-project/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import Inspect from "vite-plugin-inspect";
 import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
   esbuild: {
     jsxFactory: "create",
     jsxInject: "import { create } from '/src/12-create.js'",
   },
-  plugins: [Inspect(), tailwindcss()],
+  plugins: [Inspect(), tailwindcss(), checker({ typescript: true })],
 });
