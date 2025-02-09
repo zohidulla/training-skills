b7cf993fb18431ff629561c4bd8c85b26ceaebe5
zohidulla <zohidjonkarimov@gmail.com>
2025-02-09 15:56:25 +0500

    Building libraries with multiple entry points

--- Build Tools/Module Bundlers/Vite/vite-project/18/lib/vite.config.js
+++ Build Tools/Module Bundlers/Vite/vite-project/18/lib/vite.config.js
@@ -3,9 +3,17 @@ import { resolve } from "path";
 export default {
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.js"),
+      entry: [
+        resolve(__dirname, "src/main.js"),
+        resolve(__dirname, "src/log.js"),
+      ],
       name: "Pluck",
-      fileName: "pluck",
+      fileName: (format, name) => {
+        if (format === "es") {
+          return `${name}.js`;
+        }
+        return `${name}.${format}`;
+      },
     },
   },
 };
