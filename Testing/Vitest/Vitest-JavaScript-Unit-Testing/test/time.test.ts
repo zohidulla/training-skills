2529852332fd700916e8a463f890f92aff79a7c4
zohidulla <zohidjonkarimov@gmail.com>
2025-02-27 09:26:00 +0500

    Testing tomorrow function

--- Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/time.test.ts
+++ Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/time.test.ts
@@ -1,5 +1,5 @@
 import { expect, it, vi } from 'vitest'
-import { today } from '../src/time'
+import { today, tomorrow } from '../src/time'
 
 it('gets the current time', () => {
   const date = new Date('2025-01-01')
@@ -7,7 +7,13 @@ it('gets the current time', () => {
   expect(today()).toEqual(date)
   vi.useRealTimers()
 })
-it.todo('gets date of tomorrow')
+it('gets date of tomorrow', () => {
+  const date = new Date('2025-01-01')
+  const tomorrowDate = new Date('2025-01-02')
+  vi.setSystemTime(date)
+  expect(tomorrow()).toEqual(tomorrowDate)
+  vi.useRealTimers()
+})
 it.todo('gets end of hour date')
 it.todo('checks if passed date is today')
 it.todo('converts milliseconds to seconds')
