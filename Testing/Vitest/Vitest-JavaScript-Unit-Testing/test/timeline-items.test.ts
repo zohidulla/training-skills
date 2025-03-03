954cfcb94699b06d58b20cef5b0add0066458f93
zohidulla <zohidjonkarimov@gmail.com>
2025-03-03 09:20:23 +0500

    Testing: Updates the timeline item

---
+++ Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/timeline-items.test.ts
@@ -0,0 +1,25 @@
+import { expect, it } from 'vitest'
+import { TimelineItem } from '../src/types'
+import { updateTimelineItem } from '../src/timeline-items'
+
+it('updates the timeline item', () => {
+  const timelineItem: TimelineItem = {
+    hour: 1,
+    activityId: '1',
+    activitySeconds: 0,
+    isActive: false
+  }
+  const updatedFields: TimelineItem = {
+    hour: 1,
+    activityId: '1',
+    activitySeconds: 0,
+    isActive: true
+  }
+
+  const updatedTimelineItem = updateTimelineItem(timelineItem, updatedFields)
+
+  expect(timelineItem).toEqual(updatedFields)
+  expect(updatedTimelineItem).toEqual(updatedFields)
+})
+it.todo('resets the timeline item activities')
+it.todo('calculates the tracked activity seconds')
