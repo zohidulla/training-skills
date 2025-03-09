f9f8f830039d1c23ade4220519fe3768760a30b4
zohidulla <zohidjonkarimov@gmail.com>
2025-03-09 13:50:57 +0500

    Testing Hook beforeEach

--- Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/activities.test.ts
+++ Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/activities.test.ts
@@ -1,7 +1,17 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { calculateActivityCompletionPercentage, updateActivity } from '../src/activities'
 import { Activity } from '../src/types'
 import { HUNDRED_PERCENT, SECONDS_IN_HOUR } from '../src/constants'
+
+let activity: Activity
+
+beforeEach(() => {
+  activity = {
+    id: '1',
+    name: 'Training',
+    secondsToComplete: SECONDS_IN_HOUR * 1
+  }
+})
 
 describe('updateActivity', () => {
   const updatedFields: Activity = {
@@ -9,45 +19,22 @@ describe('updateActivity', () => {
     name: 'Reading',
     secondsToComplete: SECONDS_IN_HOUR * 2
   }
-  it('updates original activity', () => {
-    // arrange (given)
-    const activity: Activity = {
-      id: '1',
-      name: 'Training',
-      secondsToComplete: SECONDS_IN_HOUR * 1
-    }
 
-    // act (when)
+  it('updates original activity', () => {
     updateActivity(activity, updatedFields)
 
-    // assert (then)
     expect(activity).toEqual(updatedFields)
   })
 
   it('returns updated activity', () => {
-    // arrange (given)
-    const activity: Activity = {
-      id: '1',
-      name: 'Training',
-      secondsToComplete: SECONDS_IN_HOUR * 1
-    }
-    const updatedFields: Activity = {
-      id: '2',
-      name: 'Reading',
-      secondsToComplete: SECONDS_IN_HOUR * 2
-    }
-
     expect(updateActivity(activity, updatedFields)).toEqual(updatedFields)
   })
 
   it('calculates activity completion percentage', () => {
-    const activity: Activity = {
-      id: '1',
-      name: 'Training',
-      secondsToComplete: SECONDS_IN_HOUR * 1
-    }
+    expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0)).toBe(0)
+    expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0.5)).toBe(50)
+    expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 1)).toBe(
+      HUNDRED_PERCENT
+    )
   })
-  expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0)).toBe(0)
-  expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0.5)).toBe(50)
-  expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 1)).toBe(HUNDRED_PERCENT)
 })
