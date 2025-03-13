06d91750db52d2103b06111dd24b9e179f18ed59
zohidulla <zohidjonkarimov@gmail.com>
2025-03-13 09:28:50 +0500

    Testing parameterized for router and time modules

--- Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/router.test.ts
+++ Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/router.test.ts
@@ -1,14 +1,14 @@
-import { expect, it } from 'vitest'
+import { expect, it, test } from 'vitest'
 import { normalizePageHash } from '../src/router'
 import { PageName } from '../src/types'
 
-it('normalizes valid page hash', () => {
-  Object.values(PageName).forEach((page) => {
-    window.location.hash = page
-    expect(normalizePageHash()).toBe(page)
-    expect(window.location.hash).toBe(`#${page}`)
-  })
+test.each(Object.values(PageName))('normalizes valid page hash: %s', (page) => {
+  window.location.hash = page
+
+  expect(normalizePageHash()).toBe(page)
+  expect(window.location.hash).toBe(`#${page}`)
 })
+
 it('normalizes invalid page hash', () => {
   window.location.hash = 'invalid'
 
--- Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/time.test.ts
+++ Testing/Vitest/Vitest-JavaScript-Unit-Testing/test/time.test.ts
@@ -1,35 +1,49 @@
-import { expect, it, vi } from 'vitest'
+import { expect, it, test, vi } from 'vitest'
 import { endOfHour, isToday, today, tomorrow, toSeconds } from '../src/time'
 import { MILLISECONDS_IN_SECOND } from '../src/constants'
 
-it('gets the current time', () => {
-  const date = new Date('2025-01-01')
+test.each([
+  [new Date('2025-01-01'), new Date('2025-01-01')],
+  [new Date('2025-01-02'), new Date('2025-01-02')],
+  [new Date('2025-01-03'), new Date('2025-01-03')]
+])('today(%o) -> %o', (date, expectedDate) => {
   vi.setSystemTime(date)
-  expect(today()).toEqual(date)
+  expect(today()).toEqual(expectedDate)
   vi.useRealTimers()
 })
-it('gets date of tomorrow', () => {
-  const date = new Date('2025-01-01')
-  const tomorrowDate = new Date('2025-01-02')
+
+test.each([
+  [new Date('2025-01-01'), new Date('2025-01-01')],
+  [new Date('2025-01-02'), new Date('2025-01-02')],
+  [new Date('2025-01-03'), new Date('2025-01-03')]
+])('tomorrow(%o) -> %o', (date, expectedDate) => {
   vi.setSystemTime(date)
-  expect(tomorrow()).toEqual(tomorrowDate)
+  expect(tomorrow()).toEqual(expectedDate)
   vi.useRealTimers()
 })
-it('gets end of hour date', () => {
-  const date = new Date('2025-01-01T12:00:00')
-  const endOfHourDate = new Date('2025-01-01T12:59:59')
-  expect(endOfHour(date)).toEqual(endOfHourDate)
+
+test.each([
+  [new Date('2025-01-01T12:00:00'), new Date('2025-01-01T12:59:59')],
+  [new Date('2025-01-01T12:00:00'), new Date('2025-01-01T12:59:59')],
+  [new Date('2025-01-01T12:00:00'), new Date('2025-01-01T12:59:59')]
+])('endOfHour(%o) -> %o', (date, expectedDate) => {
+  expect(endOfHour(date)).toEqual(expectedDate)
 })
+
 it('checks if passed date is today', () => {
   const date = new Date('2025-01-01')
   vi.setSystemTime(date)
   expect(isToday(date)).toBe(false)
   vi.useRealTimers()
 })
-it('converts milliseconds to seconds', () => {
-  expect(toSeconds(-MILLISECONDS_IN_SECOND * 10)).toBe(-10)
-  expect(toSeconds(-MILLISECONDS_IN_SECOND * 1)).toBe(-1)
-  expect(toSeconds(MILLISECONDS_IN_SECOND * 0)).toBe(0)
-  expect(toSeconds(MILLISECONDS_IN_SECOND * 1)).toBe(1)
-  expect(toSeconds(MILLISECONDS_IN_SECOND * 10)).toBe(10)
+
+test.each([
+  [MILLISECONDS_IN_SECOND * 0, 0],
+  [MILLISECONDS_IN_SECOND * 1, 1],
+  [MILLISECONDS_IN_SECOND * 10, 10],
+  [-MILLISECONDS_IN_SECOND * 0, 0],
+  [-MILLISECONDS_IN_SECOND * 1, -1],
+  [-MILLISECONDS_IN_SECOND * 10, -10]
+])('toSeconds(%i) -> %i', (milliseconds, seconds) => {
+  expect(toSeconds(milliseconds)).toBe(seconds)
 })
