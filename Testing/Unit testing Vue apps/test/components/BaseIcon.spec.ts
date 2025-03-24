805c18805a1b875c6d0b1334dad637259b89df90
zohidulla <zohidjonkarimov@gmail.com>
2025-03-24 09:28:28 +0500

    Test Base Icon with toMatchSnapshot

--- Testing/Unit testing Vue apps/test/components/BaseIcon.spec.ts
+++ Testing/Unit testing Vue apps/test/components/BaseIcon.spec.ts
@@ -1,7 +1,8 @@
 import { expect, it } from 'vitest'
-import { shallowMount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import { IconName } from '../../src/types'
 import BaseIcon from '../../src/components/BaseIcon.vue'
+import { ICONS } from '../../src/icons'
 
 it('renders icon with default classes', () => {
   const wrapper = shallowMount(BaseIcon, {
@@ -19,4 +20,12 @@ it('renders icon with custom classes', () => {
   })
 
   expect(wrapper.classes().join(' ')).toBe(classes)
+})
+
+it.each(Object.keys(ICONS))('renders %s icon', (name) => {
+  const wrapper = mount(BaseIcon, {
+    props: { name: name as IconName }
+  })
+
+  expect(wrapper.html()).toMatchSnapshot()
 })
