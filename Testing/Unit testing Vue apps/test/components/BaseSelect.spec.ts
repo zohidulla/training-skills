81ba936dd155bed04269f064cdfc5c085d2990c0
zohidulla <zohidjonkarimov@gmail.com>
2025-03-26 09:29:55 +0500

    Testing BaseSelect component

--- Testing/Unit testing Vue apps/test/components/BaseSelect.spec.ts
+++ Testing/Unit testing Vue apps/test/components/BaseSelect.spec.ts
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import { it, expect } from 'vitest'
 import { ButtonType, IconName } from '../../src/types'
 import BaseSelect from '../../src/components/BaseSelect.vue'
@@ -17,5 +17,36 @@ it('renders select with reset button', () => {
   expect(wrapper.findComponent(BaseButton).vm.type).toBe(ButtonType.NEUTRAL)
   expect(wrapper.findComponent(BaseIcon).vm.name).toBe(IconName.X_MARK)
 })
-it.todo('renders select with placeholder')
-it.todo('renders select with options')
+
+it('renders select with placeholder', () => {
+  const placeholder = 'Placeholder'
+  const wrapper = shallowMount(BaseSelect, {
+    props: {
+      placeholder,
+      options: [],
+      selected: null
+    }
+  })
+
+  expect(wrapper.text()).toContain(placeholder)
+})
+
+it('renders select with options', () => {
+  const options = [
+    { value: '1', label: 'Training' },
+    { value: '2', label: 'Reading' },
+    { value: '3', label: 'Coding' }
+  ]
+  const wrapper = shallowMount(BaseSelect, {
+    props: {
+      placeholder: '',
+      options,
+      selected: null
+    }
+  })
+
+  options.forEach(({ value, label }) => {
+    expect(wrapper.find(`option[value=${value}]`).exists()).toBe(true)
+    expect(wrapper.find('select').text()).toContain(label)
+  })
+})
