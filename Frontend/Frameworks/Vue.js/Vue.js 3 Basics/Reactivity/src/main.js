3e977df1e94ed49745a742a0c5d8cf0137d545bf
zohidulla <zohidjonkarimov@gmail.com>
2025-04-01 16:31:48 +0500

    Fine-grained reactivity

--- Frontend/Frameworks/Vue.js/Vue.js 3 Basics/Reactivity/src/main.js
+++ Frontend/Frameworks/Vue.js/Vue.js 3 Basics/Reactivity/src/main.js
@@ -1,21 +1,25 @@
-import { ref, watchEffect } from "vue";
+import { computed, ref, watchEffect } from "vue";
 
 const counterButton = document.querySelector("button#counter");
 const resetButton = document.querySelector("button#reset");
 
 const counterState = ref(5);
+const isCounterTooBig = computed(() => counterState.value > 10);
 
 watchEffect(() => {
   renderCounter();
 });
 
+watchEffect(() => {
+  updateCounterColor();
+});
+
 function renderCounter() {
   counterButton.textContent = `Counter: ${counterState.value}`;
-  counterButton.classList.toggle("red", isCounterTooBig());
 }
 
-function isCounterTooBig() {
-  return counterState.value > 10;
+function updateCounterColor() {
+  counterButton.classList.toggle("red", isCounterTooBig.value);
 }
 
 counterButton.addEventListener("click", () => {
