9c7412bd628b7d7f6127d8c8b5290b233ca769c9
zohidulla <zohidjonkarimov@gmail.com>
2025-04-10 06:24:15 +0500

    Refactoring ActivityItem.test.ts

--- Testing/Unit testing Vue apps/test/components/ActivityItem.test.ts
+++ Testing/Unit testing Vue apps/test/components/ActivityItem.test.ts
@@ -2,21 +2,30 @@ import { expect, it, vi } from 'vitest'
 import ActivityItem from '../../src/components/ActivityItem.vue'
 import { mount } from '@vue/test-utils'
 import BaseButton from '../../src/components/BaseButton.vue'
-import { ButtonType, IconName } from '../../src/types'
+import { Activity, ButtonType, IconName } from '../../src/types'
 import BaseIcon from '../../src/components/BaseIcon.vue'
 import * as timelineItems from '../../src/timeline-items'
 import * as activities from '../../src/activities'
 
-it('has delete button', () => {
-  const wrapper = mount(ActivityItem, {
+function mountActivityItem(activityOverrides: Partial<Activity> = {}) {
+  return mount(ActivityItem, {
     props: {
-      activity: {
-        id: '',
-        name: '',
-        secondsToComplete: 0
-      }
+      activity: createActivity(activityOverrides)
     }
   })
+}
+
+function createActivity(overrides: Partial<Activity> = {}) {
+  return {
+    id: '',
+    name: '',
+    secondsToComplete: 0,
+    ...overrides
+  }
+}
+
+it('has delete button', () => {
+  const wrapper = mountActivityItem()
 
   expect(wrapper.findComponent(BaseButton).props('type')).toBe(ButtonType.DANGER)
   expect(wrapper.findComponent(BaseIcon).props('name')).toBe(IconName.TRASH)
@@ -25,14 +34,8 @@ it('has delete button', () => {
 it('deletes activity', () => {
   const resetTimelineItemActivitiesSpy = vi.spyOn(timelineItems, 'resetTimelineItemActivities')
   const deleteActivitySpy = vi.spyOn(activities, 'deleteActivity')
-  const activity = {
-    id: '',
-    name: '',
-    secondsToComplete: 0
-  }
-  const wrapper = mount(ActivityItem, {
-    props: { activity }
-  })
+  const activity = createActivity()
+  const wrapper = mountActivityItem(activity)
 
   wrapper.find('button').trigger('click')
 
@@ -41,4 +44,10 @@ it('deletes activity', () => {
   expect(deleteActivitySpy).toBeCalledTimes(1)
   expect(deleteActivitySpy).toBeCalledWith(activity)
   vi.resetAllMocks()
+})
+
+it('shows activity nam', () => {
+  const name = 'Reading'
+
+  expect(mountActivityItem({ name }).text()).toContain(name)
 })
