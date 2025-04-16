340a115e442f703bc913e2ec1c8b9b9da6458059
zohidulla <zohidjonkarimov@gmail.com>
2025-04-16 06:39:03 +0500

    Test NavItem component

--- Testing/Unit testing Vue apps/test/components/NavItem.test.ts
+++ Testing/Unit testing Vue apps/test/components/NavItem.test.ts
@@ -1,8 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
+import { flushPromises, shallowMount } from '@vue/test-utils'
 import NavItem from '../../src/components/NavItem.vue'
 import { NAV_ITEMS } from '../../src/constants'
 import { it, expect } from 'vitest'
 import BaseIcon from '../../src/components/BaseIcon.vue'
+import { currentPage } from '../../src/router'
+import { PageName } from '../../src/types'
 
 const timelineNavItem = NAV_ITEMS[0]
 
@@ -16,4 +18,21 @@ it('renders nav item', () => {
   expect(wrapper.text()).toContain(timelineNavItem.page)
   expect(wrapper.findComponent(BaseIcon).props('name')).toEqual(timelineNavItem.icon)
   expect(wrapper.find('a').attributes('href')).toBe(`#${timelineNavItem.page}`)
+})
+
+it('has hover state if nav item does not correspond to current page', async () => {
+  currentPage.value = PageName.TIMELINE
+  const hoverClass = 'hover:bg-gray-100'
+  const wrapper = shallowMount(NavItem, {
+    props: {
+      navItem: timelineNavItem
+    }
+  })
+
+  expect(wrapper.find('a').classes()).not.toContain(hoverClass)
+
+  currentPage.value = PageName.ACTIVITIES
+  await flushPromises()
+
+  expect(wrapper.find('a').classes()).toContain(hoverClass)
 })
