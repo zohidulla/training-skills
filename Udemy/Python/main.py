a4a4d9b381e9660c15c499b0f406673a05b0b4cd
zohidulla <zohidjonkarimov@gmail.com>
2025-08-31 10:25:46 +0500

    Practice bool

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -78,3 +78,22 @@
 # print(complex_a + complex_b)
 
 # (10 + 7j)(3 + 3j) = 30 + 30j + 21j - 21 = 30 + 51j
+
+# db_is_available = False
+# print(db_is_available)
+# print(type(db_is_available))
+
+# db_is_available = True
+# print(db_is_available)
+
+# print(bool(10))
+# print(bool('abc'))
+# print(bool([]))
+# print(bool([1, 2]))
+# print(bool({}))
+# print(bool(None))
+
+# print(100 > 10)
+# print('Long string' > 'Short')
+# print([] == [])
+# print({'a': 3} == {'a': 5})
