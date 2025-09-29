4f42acb1f9c98b6d8059740c04fc614cc98a182c
zohidulla <zohidjonkarimov@gmail.com>
2025-09-29 11:09:32 +0500

    Tasks set

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -453,8 +453,29 @@
 
 # print(my_set.symmetric_difference(copied_set))  # {'l', 't'}
 
-a = {'abc', 'd', 'f', 'y'}
-b = {'abc', 'd', 'f', 'l'}
+# a = {'abc', 'd', 'f', 'y'}
+# b = {'abc', 'd', 'f', 'l'}
 
-print((a | b) - (a & b))  # {'y', 'l'}
-print(a.symmetric_difference(b))  # {'y', 'l'}
+# print((a | b) - (a & b))  # {'y', 'l'}
+# print(a.symmetric_difference(b))  # {'y', 'l'}
+
+# Задача
+# 1. Создайте набор с несколькиx элементов типа int
+# 2. Добавьте в него еще один элемент
+# 3. Создайте еще один набор с несколькими элементами, причем некоторые должны быть такими же как в первом наборе
+# 4. Найдите общие элементы в двух наборах и поместите их в новый набор
+# 5. Конвертируйте результирующий набор в список и выведите список  в терминал
+
+set_one = {10, 5, 7, 15, 100}
+set_one.add(200)
+
+set_two = {20, 7, 300, 100, 200}
+
+set_common = set_one.intersection(set_two)
+print(set_common)  # {200, 100, 7}
+
+list_common = list(set_common)
+print(list_common)  # [200, 100, 7]
+
+print(set_one)  # {100, 5, 7, 10, 200, 15}
+print(set_two)  # {200, 100, 20, 7, 300}
