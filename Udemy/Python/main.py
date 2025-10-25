205172952b8cbc1777425ef37cd9ae1d2107dc1a
zohidulla <zohidjonkarimov@gmail.com>
2025-10-25 17:54:40 +0500

    Practice

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -756,3 +756,38 @@
 # # {'id': 243, 'author': 'Alice', 'created_on_weekday': 'Wednesday'}
 # print(initial_post)
 # # {'id': 243, 'author': 'Alice'}
+
+# Колбэк функции
+# def print_number_info(num):
+#     if num % 2 == 0:
+#         print(f"{num} is even")
+#     else:
+#         print(f"{num} is odd")
+
+
+# def print_square_num(num):
+#     print(f"The square of {num} is {num * num}")
+
+
+# def process_number(number, callback_fn):
+#     callback_fn(number)
+
+
+# entered_number = int(input("Enter any number: "))
+
+# process_number(entered_number, print_number_info)
+# process_number(entered_number, print_square_num)
+
+
+# def send_data(data):
+#     # sending data to the remote server
+#     pass
+
+
+# def process_data(input_data, send_data_fn):
+#     updated_data = input_data.copy()
+#     # actions with updated_data
+#     send_data_fn(updated_data)
+
+
+# process_data({'name': 'Alice'}, send_data)
