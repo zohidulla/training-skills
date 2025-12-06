08a46cd915bcf329759f8a2ea29735b02e2fce8a
zohidulla <zohidjonkarimov@gmail.com>
2025-12-06 22:46:39 +0500

    The if Statement

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -1142,3 +1142,16 @@
 
 
 # ************************************************************************************************************************
+# # Инструкция if
+# num_one = 10
+# num_two = 5
+
+# if (num_one > 0 and
+#     num_two > 0 and
+#     isinstance(num_one, int) and
+#         isinstance(num_two, int)):
+#     print("Both numbers are positive integers")
+#     # Both numbers are positive integers
+
+
+# ************************************************************************************************************************
