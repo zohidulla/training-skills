1493ed8f8c65e4aad1d98f40b0a4503313b8a88b
zohidulla <zohidjonkarimov@gmail.com>
2025-12-10 09:22:39 +0500

    Using if in Functions

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -1171,3 +1171,23 @@
 # else:
 #     print("There is no phone brand")
 #     # There is no phone brand
+
+
+# ************************************************************************************************************************
+# # Использование if в функциях
+
+# def nums_info(a, b):
+#     if (type(a) is not int) or (type(b) is not int):
+#         return "One of the arguments is not an integer"
+
+#     if a >= b:
+#         return f"{a} is greater than or equal to {b}"
+
+#     return f"{a} is less than {b}"
+
+# print(nums_info(True, 10))  # One of the arguments is not an integer
+# print(nums_info(10, 2))   # 110 is greater than or equal to 2
+# print(nums_info(5, 15))   # 5 is less than 15
+
+
+# ************************************************************************************************************************
