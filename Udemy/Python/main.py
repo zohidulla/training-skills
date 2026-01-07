a1529fbaf4f0f7fc90709828ac0801a91834944d
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-07 16:47:40 +0500

    Practice: Magic Methods in Classes

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -1496,3 +1496,33 @@
 
 
 # ************************************************************************************************************************
+## Магические методы в классах
+# class Comment:
+#     def __init__(self, text):
+#         self.text = text
+#         self.votes_qty = 0
+
+#     def upvote(self):
+#         self.votes_qty += 1
+
+#     def __add__(self, other):
+#         return (f"{self.text} {other.text}", self.votes_qty + other.votes_qty)
+
+#     def __eq__(self, other):
+#       return self.text == other.text and self.votes_qty == other.votes_qty
+
+# first_comment = Comment("First comment")
+# first_comment.upvote()
+# second_comment = Comment("Second comment")
+# second_comment.upvote()
+# print(first_comment + second_comment)
+# # ('First comment Second comment', 2)
+
+# print(first_comment == second_comment)  # False
+
+# third_comment = Comment("First comment")
+# third_comment.upvote()
+# print(first_comment == third_comment)  # True
+
+# ************************************************************************************************************************
+
