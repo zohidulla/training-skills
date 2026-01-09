08036cb80f8b21f195d17f60dc53ef083ed2b728
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-09 10:13:42 +0500

    Decorators

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -1786,3 +1786,14 @@
 #         pass
 
 # ************************************************************************************************************************
+# Декораторы
+def decorator_function(original_function):
+    def wrapper_function():
+        return original_function()
+    return wrapper_function
+
+@decorator_function
+def my_function():
+    print("This is my function")
+
+my_function()
