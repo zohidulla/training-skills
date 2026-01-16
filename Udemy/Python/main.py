5cc5933d83c1d3f1cdd073ef0721c2cec1af9080
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-16 10:46:23 +0500

    Practice: JSON

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -2047,3 +2047,15 @@
 
 
 # **********************************************************************************************************************************
+# JSON
+import json
+
+json_str = '{"id": 235, "brand": "Nike", "qty": 84, "status": {"isForSale": true}}'
+
+sneakers = json.loads(json_str)
+
+print(type(sneakers))  # <class 'dict'>
+print(sneakers) # {'id': 235, 'brand': 'Nike', 'qty': 84, 'status': {'isForSale': True}}
+print(sneakers['brand'])  # Nike
+print(sneakers['qty'])    # 84
+print(sneakers['status']['isForSale'])  # True
