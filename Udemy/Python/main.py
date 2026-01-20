ea2f892869e62044f201beebd2f99b60d1d9baa2
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-20 16:52:06 +0500

    Modules for Working with Numbers: secrets

--- Udemy/Python/main.py
+++ Udemy/Python/main.py
@@ -2463,8 +2463,9 @@ from pathlib import Path
 # print(random.choices('0123456789ABCDEF', k=6))  # e.g., ['A', '3', 'F', '1', 'C', '9']
 # print(''.join(random.choices('0123456789ABCDEFJHKL', k=8)))  # e.g., '3F1AC9JL'
 
-import secrets
-import string
+# -----------------------------------------------------------
+# import secrets
+# import string
 
 # print(string.ascii_letters)  # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 # print(string.ascii_lowercase)  # abcdefghijklmnopqrstuvwxyz
@@ -2474,6 +2475,27 @@ import string
 # print(string.punctuation)  # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 # secure_password = ''.join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(12))
 # print(secure_password)  # e.g., 'aB3$dE5&gH1!'
-all_chars = string.ascii_letters + string.digits + string.punctuation
-secure_password = ''.join(secrets.choice(all_chars) for _ in range(10))
-print(secure_password)  # e.g., 'F3$dE5&gH1'
+# all_chars = string.ascii_letters + string.digits + string.punctuation
+# secure_password = ''.join(secrets.choice(all_chars) for _ in range(10))
+# print(secure_password)  # e.g., 'F3$dE5&gH1'
+
+# -----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# **********************************************************************************************************************************
