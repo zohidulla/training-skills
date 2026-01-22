20324a43fbcb683a6e31440c9f68ce0c7c489660
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-22 14:41:56 +0500

    Adding application routes to project routes

--- Udemy/Python/django-courses/shop/views.py
+++ Udemy/Python/django-courses/shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return render(request, 'shop/index.html')
+    return HttpResponse("Welcome to the Shop Index Page")
