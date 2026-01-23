5f0c88388704cab47a6d732f110d77f069e7d707
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-23 10:45:40 +0500

    Add __str__ methods to Category and Course models for better string representation

--- Udemy/Python/django-courses/shop/models.py
+++ Udemy/Python/django-courses/shop/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.title
+
 
 class Course(models.Model):
     title = models.CharField(max_length=300)
@@ -14,3 +17,7 @@ class Course(models.Model):
     reviews_qty = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
