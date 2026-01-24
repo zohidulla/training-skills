ed15c59ae9397b76be6496735c110bb3dbf43b01
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-24 12:09:52 +0500

    Refactor course templates and views for improved structure and functionality

--- Udemy/Python/django-courses/shop/views.py
+++ Udemy/Python/django-courses/shop/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Course, Category
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+    # # OPTION 1
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'shop/single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+
+    # OPTION 2
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {'course': course})
