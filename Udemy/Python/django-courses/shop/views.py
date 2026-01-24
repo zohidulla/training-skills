a4f9c8e4b9bdcc70b428d48f565b5aabe42cf07a
Zohid Karim <58090046+zohidulla@users.noreply.github.com>
2026-01-24 14:41:31 +0500

    Refactor template structure by adding base template and updating course views

--- Udemy/Python/django-courses/shop/views.py
+++ Udemy/Python/django-courses/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Course, Category
+from .models import Course
 
 def index(request):
     courses = Course.objects.all()
